1d32383c50f88d331392b0b7a4867f915a251ab1
PaDDy <paddygan10@gmail.com>
2022-08-25 16:56:43 +0200

    Display_Position2

--- app/src/PlayersBorders.tsx
+++ app/src/PlayersBorders.tsx
@@ -41,7 +41,7 @@ const bordersPlayers = [
     [{height: 0.1, width: 44, top: 53.5, left:0},{height: 0.1, width: 44, top: 53.5, left: 132}],
     [{height: 0.1, width: 44, top: 53.5, left:0},{height: 0.1, width: 44, top: 53.5, left: 132},{height: 26, width: 0.1, top: 7, left : 50*screenRatio}],
     // [{height: 26, width: 0.1, top: 7, left: 44},{height: 40, width: 0.1, top: 60, left: 44},{height: 26, width: 0.1, top: 7, left: 132},{height: 40, width: 0.1, top: 60, left: 132}],
-    [{height: 26, width: 0.2, top: 7, left: 44, transOrigin: "bottom", skewX: 55},{height: 40, width: 0.2, top: 60, left: 44, transOrigin : "top", skewX: -45, zIndex: 3},{height: 26, width: 0.2, top: 7, left: 132, transOrigin: "bottom", skewX: 125},{height: 40, width: 0.2, top: 60, left: 132, transOrigin: "top", skewX: 35}],
+    [{height: 26, width: 0.2, top: 7, left: 44, transOrigin: "bottom", skewX: 55},{height: 40, width: 0.2, top: 60, left: 44, transOrigin : "top", skewX: -45, zIndex: 3},{height: 26, width: 0.2, top: 7, left: 132, transOrigin: "bottom", skewX: 125},{height: 40, width: 0.2, top: 60, left: 132, transOrigin: "top", skewX: 45}],
     [{height: 0.1, width: 44, top: 53.5, left:0},{height: 0.1, width: 44, top: 53.5, left: 132},{height: 26, width: 0.1, top: 7, left : 50*screenRatio},{height: 40, width: 0.1, top: 60, left : 44},{height: 40, width: 0.1, top: 60, left : 132}],
     [{height: 0.1, width: 44, top: 53.5, left:0},{height: 0.1, width: 44, top: 53.5, left: 132},{height: 93, width: 0.1, top: 7, left:44},{height: 93, width: 0.1, top: 7, left: 132}]
 ]
