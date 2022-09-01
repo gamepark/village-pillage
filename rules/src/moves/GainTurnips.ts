f532392afde57f73a1ed32591763f4ef4e0946e9
PaDDy <paddygan10@gmail.com>
2022-09-01 10:32:28 +0200

    GainTurnips_Move

---
+++ rules/src/moves/GainTurnips.ts
@@ -0,0 +1,22 @@
+import GameState, {getPlayerState} from '../GameState'
+import MoveType from './MoveType'
+
+/**
+ * An example of a simple move: one player returns a quantity of gold to the bank
+ */
+type GainTurnips = {
+  type: MoveType.GainTurnips
+  playerId: number
+  quantity: number
+}
+
+export default GainTurnips
+
+export function gainTurnipsMove(playerId: number, quantity: number) : GainTurnips {
+  return {type: MoveType.GainTurnips, playerId, quantity}
+}
+
+export function gainTurnips(state: GameState, move: GainTurnips) {
+  const player = getPlayerState(state, move.playerId)
+  player.stock += move.quantity
+}
